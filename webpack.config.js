7b75c3ba4566a8972f1c326c813eb779c6925ed3
Brian Johnson <johnson8727@gmail.com>
2021-08-20 22:34:22 -0400

    configured webpack with 2 new scripts, add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        publicPath: '/'
     },
+    devServer: {
+        static: './dist',
+    },
+    devtool: 'inline-source-map',
     module: {
         rules: [
-            +      {
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
         ],
     },
-};
+}
