0ef1dc73103a68179a83a1bb2fc5a54516acce90
Brian Johnson <johnson8727@gmail.com>
2021-08-20 22:07:56 -0400

    added webpack along with a couple loaders

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            +      {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
