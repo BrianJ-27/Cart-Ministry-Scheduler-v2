e42e181163dd04569bd26a46c68ff9d745e54d6c
Nestor Padilla <nrpadilla2@gmail.com>
2021-08-29 16:25:53 -0400

    added npm run all and a temporary call to the mock api server for testing that it all works

---
+++ webpack.config.dev.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const host = process.env.HOST || 'localhost';
+// Required for babel-preset-react-app
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  //todo what is the right devtool to use? inline-source-map, source-map, eval or cheap-module-source-map?
+  devtool: 'cheap-module-source-map',
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    compress: true,
+    hot: true,
+    host,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.API_URL': JSON.stringify('http://localhost:3001')
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      minify: true
+    })
+  ]
+};
