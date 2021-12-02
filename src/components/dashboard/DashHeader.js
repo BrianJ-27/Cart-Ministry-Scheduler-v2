32a3d3745936b3f3e724c44a6b50d84657214e70
Brian Johnson <johnson8727@gmail.com>
2021-12-01 19:49:51 -0500

    Finished laying out UI Dashboard. Working on the tests for the components I built

--- src/components/dashboard/DashHeader.js
+++ src/components/dashboard/DashHeader.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import ProfileBlock from "../profile/index";
+import MediaQuery from "react-responsive";
 
-const DashHeader = ({ kingdomHall }) => {
-    return (
-        <header className="header__container">
-            <h2>{kingdomHall}</h2>
-            <p>Congregation</p>
-        </header>
-    );
-}
+const DashHeader = ({ userProfile }) => {
+  return (
+    <header className="header__container">
+      <MediaQuery maxWidth={767}>
+        <div style={{ width: "100%" }}>
+          <ProfileBlock userProfile={userProfile} />
+          <p style={{ marginTop: ".5rem", color: '#fff' }}>
+            {userProfile.congregation} &#124;
+            <span>Congregation</span>
+          </p>
+        </div>
+      </MediaQuery>
+      <MediaQuery minWidth={768}>
+        <h2>{userProfile.congregation}</h2>
+        <p>Congregation</p>
+      </MediaQuery>
+    </header>
+  );
+};
 
 export default DashHeader;
